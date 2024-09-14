24c6c734abcef8677c22a41b3c240b557a171ce5
Renato Carvalho <renatocarvalho01234@gmail.com>
2024-09-13 22:14:19 -0300

    feat:no route

--- server/app.py
+++ server/app.py
@@ -2,21 +2,5 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/hello', methods=["GET"])
-def hello():
-    return 'Hello, World!'
-
-@app.route('/Girino', methods=["GET"])
-def girino():
-    return 'Hello, Girino'
-    
-@app.route('/renato', methods=['GET'])
-def peixe():
-    return 'Peixe espada'
-
-@app.route('/lucas', methods=["GET"])
-def lucas():
-    return 'O sapo que lava o pé'
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)

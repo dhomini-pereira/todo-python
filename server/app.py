334a61a01bad1baa973a3057c72f8425b357e43a
Gabriel-Reis021 <gabrielreissantos180@gmail.com>
2024-09-13 21:41:35 -0300

    trocando rota

--- server/app.py
+++ server/app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello():
     return 'Hello, World!'
 
+@app.route('/Girino', methods=["GET"])
+def girino():
+    return 'Hello, Girino'
+    
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)

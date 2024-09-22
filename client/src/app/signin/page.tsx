801b7ad916f37fb234816cd3a08ec90385d77ea8
magnoDuraes <magnodbf962@gmail.com>
2024-09-22 00:08:39 -0300

    feat: Adicionando conteudo na home | Adicionando cors no backend | Adicionando fetch no signUp e signIn

--- client/src/app/signin/page.tsx
+++ client/src/app/signin/page.tsx
@@ -10,8 +10,25 @@ type IUser = {
 export default function Page() {
   const { handleSubmit, register } = useForm<IUser>();
 
-  function handleSignIn(e: IUser) {
-    console.log(e);
+  async function handleSignIn(e: IUser) {
+    try {
+      const apiUrl = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000";
+      const response = await fetch(`${apiUrl}/login`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(e),
+      });
+      const data = await response.json();
+      if (response.ok) {
+        sessionStorage.setItem("token", data.token);
+      } else {
+        alert("Ocorreu um erro, tente novamente mais tarde.");
+      }
+    } catch {
+      alert("Ocorreu um erro, tente novamente mais tarde.");
+    }
   }
 
   return (
@@ -20,16 +37,14 @@ export default function Page() {
         <meta name="theme-color" content="#030B19" />
       </header>
       <div
-        className="bg-cover bg-center h-screen w-full flex items-center justify-center flex-col font-body text-slate-200"
-        style={{ backgroundImage: `url(${background.src})` }}
+        className="bg-cover bg-center w-full flex items-center justify-center flex-col font-body text-slate-200"
+        style={{ backgroundImage: `url(${background.src})`, height: "100dvh" }}
       >
         <div className="max-sm:px-6 max-sm:max-w-xl max-sm:py-6 max-sm:w-5/6 w-3/5 h-fit max-w-md bg-zinc-900 rounded-3xl py-9 px-12">
-          {/* SignUp Container */}
           <form
             className="grid gap-5 items-center"
             onSubmit={handleSubmit(handleSignIn)}
           >
-            {/* SignUp Content */}
             <div>
               <h1 className="text-4xl">Sign in</h1>
               <p className="font-light text-slate-400">
@@ -39,10 +54,12 @@ export default function Page() {
             <input
               type="email"
               className="bg-zinc-900 border-2 border-blue-800 rounded-md h-10 outline-none focus:border-blue-950 indent-3 ease-in duration-200"
+              autoComplete="off"
               placeholder="E-mail"
               {...register("email", { required: true })}
             />
             <input
+              autoComplete="off"
               type="password"
               className="bg-zinc-900 border-2 border-blue-800 rounded-md h-10 outline-none focus:border-blue-950 indent-3 ease-in duration-200"
               placeholder="Password"

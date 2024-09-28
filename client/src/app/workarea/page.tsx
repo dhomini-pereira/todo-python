bde7460f3a6d092af090a1f8eed26716ba0fd3a3
magnoDuraes <magnodbf962@gmail.com>
2024-09-27 21:27:04 -0300

    feat: Adicionando Navbar e Sidebar

---
+++ client/src/app/workarea/page.tsx
@@ -0,0 +1,11 @@
+import Navbar from "@/components/Navbar";
+import React from "react";
+
+export default function page() {
+  return (
+    <div className="h-full bg-slate-900">
+      <Navbar />
+      <h1>Conteúdo</h1>
+    </div>
+  );
+}

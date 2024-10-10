c85ed3dd7bd9e107fed2e98b6684d3e665be246a
magnoDuraes <magnodbf962@gmail.com>
2024-10-10 00:46:34 -0300

    fix: responsividade da foto de usuário na navbar foi ajustada.

--- client/src/components/navbar/Navbar.tsx
+++ client/src/components/navbar/Navbar.tsx
@@ -41,7 +41,7 @@ export default function Navbar() {
           <img
             src={user?.image_url}
             alt={user?.username}
-            className="h-9 rounded-full"
+            className="h-9 w-9 object-cover rounded-full"
           />
         ) : (
           <svg

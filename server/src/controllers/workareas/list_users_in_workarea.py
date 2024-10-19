c5ea543e671e7bc0fbc77bf2d28ec6aebf6a1ada
dhpe <dhpe@brasilcap.com.br>
2024-10-19 03:31:18 -0300

    fix: resolvendo B.O.

--- server/src/controllers/workareas/list_users_in_workarea.py
+++ server/src/controllers/workareas/list_users_in_workarea.py
@@ -4,35 +4,29 @@ from src.models.work_area import WorkArea
 from src.models.user import User
 from flask import jsonify
 
-def list_users_in_workarea(userId, workarea_id):
+def list_users_in_workarea(user_id, workarea_id):
     try:
         users = (
             User
             .select()
-            .join(MemberWorkArea, JOIN.LEFT_OUTER)
-            .join(WorkArea, JOIN.LEFT_OUTER)
+            .join(MemberWorkArea, JOIN.LEFT_OUTER, on=(MemberWorkArea.user == User.id))
+            .join(WorkArea, JOIN.LEFT_OUTER, on=(WorkArea.id == MemberWorkArea.work_area))
             .where(
-                (
-                    WorkArea.owner == userId,
-                    WorkArea.id == workarea_id
-                )|
-                (
-                    MemberWorkArea.user == userId,
-                    WorkArea.id == workarea_id
-                )
+                ((WorkArea.owner == user_id) & (WorkArea.id == workarea_id)) |
+                ((MemberWorkArea.user == user_id) & (WorkArea.id == workarea_id))
             )
         )
+
+        user_list = [
+            {
+                'id': user.id,
+                'username': user.username,
+                'email': user.email,
+                'image_url': user.image_url,
+            } for user in users
+        ]
         
-        return jsonify({
-            'users': [
-                {
-                    'id': user.id,
-                    'username': user.username,
-                    'email': user.email,
-                    'image_url': user.image_url,
-                } for user in users
-            ]
-        }), 200
+        return jsonify({'users': user_list}), 200
     
     except Exception as e:
         return jsonify({'error': f'Ocorreu um erro inesperado: {str(e)}'}), 500

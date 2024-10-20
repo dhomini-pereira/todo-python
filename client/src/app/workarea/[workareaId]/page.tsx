2ef595dced9c2dae7c9d93fb20a12932e15bc622
magnoDuraes <magnodbf962@gmail.com>
2024-10-19 22:43:58 -0300

    feat: foi adicionado a possibilidade de criar tasks por meio de modal em '/workarea/:id'

--- client/src/app/workarea/[workareaId]/page.tsx
+++ client/src/app/workarea/[workareaId]/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { API_URL } from "@/app/globals";
 import Navbar from "@/components/navbar/Navbar";
+import TaskCreate from "@/components/task/TaskCreate";
 import { useNavbar } from "@/context/NavbarContext";
 import api from "@/services/api.service";
 import { useParams, useSearchParams } from "next/navigation";
@@ -11,6 +12,7 @@ import {
   Droppable,
   DropResult,
 } from "react-beautiful-dnd";
+import { useForm } from "react-hook-form";
 
 enum TaskStatus {
   PENDING = "PENDING",
@@ -35,11 +37,20 @@ type IPromiseHttpResponse = {
   done: ITask[];
 };
 
+type ITaskHttp = {
+  title: string;
+  description: string;
+  timeEstimate: Date;
+  userId: number;
+};
+
 export default function WorkAreaInfo() {
+  const { handleSubmit, register } = useForm<ITaskHttp>();
   const { workareaId } = useParams();
   const query = useSearchParams();
   const page = query.get("page") || 1;
   const [title, setTitle] = useState<string>("");
+  const [modalShow, setModalShow] = useState(false);
   const [tasks, setTasks] = useState<IPromiseHttpResponse>({
     pending: [],
     progressing: [],
@@ -47,10 +58,29 @@ export default function WorkAreaInfo() {
   });
   const { isActive } = useNavbar();
   const [hydrated, setHydrated] = useState(false);
+  const [users, setUsers] = useState<any>([]);
 
   useEffect(() => {
     setHydrated(true);
   }, []);
+
+  async function handleTask(task: ITaskHttp) {
+    try {
+      const url = `${API_URL}/workarea/${workareaId}/task`;
+      const createdTask = await api.post(url, task);
+      setTasks((prevState) => {
+        return {
+          pending: [createdTask.data, ...prevState.pending],
+          progressing: prevState.progressing,
+          done: prevState.done,
+        };
+      });
+      alert("Task criada com sucesso!");
+      setModalShow(false)
+    } catch (err: any) {
+      alert(err.response.data.error);
+    }
+  }
 
   useEffect(() => {
     if (!hydrated) return;
@@ -58,31 +88,36 @@ export default function WorkAreaInfo() {
     (async () => {
       try {
         const url = `${API_URL}/workarea/${workareaId}/task?page=${page}`;
-        const [pendingTasks, progressingTasks, doneTasks, workarea] =
-          await Promise.all([
-            api.get(url + "&status=PENDING").catch((err) => {
-              if (err.response?.status === 404) {
-                return { data: { tasks: [] } };
-              }
-              throw err;
-            }),
-            api.get(url + "&status=PROGRESSING").catch((err) => {
-              if (err.response?.status === 404) {
-                return { data: { tasks: [] } };
-              }
-              throw err;
-            }),
-            api.get(url + "&status=DONE").catch((err) => {
-              if (err.response?.status === 404) {
-                return { data: { tasks: [] } };
-              }
-              throw err;
-            }),
-            api.get(`${API_URL}/workarea/${workareaId}`),
-          ]);
-
+        const [
+          pendingTasks,
+          progressingTasks,
+          doneTasks,
+          workarea,
+          usersWorkarea,
+        ] = await Promise.all([
+          api.get(url + "&status=PENDING").catch((err) => {
+            if (err.response?.status === 404) {
+              return { data: { tasks: [] } };
+            }
+            throw err;
+          }),
+          api.get(url + "&status=PROGRESSING").catch((err) => {
+            if (err.response?.status === 404) {
+              return { data: { tasks: [] } };
+            }
+            throw err;
+          }),
+          api.get(url + "&status=DONE").catch((err) => {
+            if (err.response?.status === 404) {
+              return { data: { tasks: [] } };
+            }
+            throw err;
+          }),
+          api.get(`${API_URL}/workarea/${workareaId}`),
+          api.get(`${API_URL}/workarea/${workareaId}/member`),
+        ]);
+        setUsers(usersWorkarea.data.users);
         setTitle(workarea.data.name);
-        // setTitle("Titulo Aqui");
 
         const httpResponse: IPromiseHttpResponse = {
           pending: pendingTasks.data.tasks,
@@ -130,7 +165,6 @@ export default function WorkAreaInfo() {
 
     try {
       const url = `${API_URL}/workarea/${workareaId}/task/${movedTask.id}`;
-
       await api.put(url, {
         status:
           TaskStatus[
@@ -149,6 +183,25 @@ export default function WorkAreaInfo() {
   return (
     <div className="h-full">
       <Navbar />
+      <TaskCreate
+        handleSubmit={{ handle: handleSubmit(handleTask), modalShow, setModalShow }}
+        register={register}
+        users={users}
+        trigger={
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            className="h-[64px] hover:text-slate-300 cursor-pointer text-slate-200"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M10 18a8 8 0 1 0 0-16 8 8 0 0 0 0 16Zm.75-11.25a.75.75 0 0 0-1.5 0v2.5h-2.5a.75.75 0 0 0 0 1.5h2.5v2.5a.75.75 0 0 0 1.5 0v-2.5h2.5a.75.75 0 0 0 0-1.5h-2.5v-2.5Z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        }
+      />
       <div
         className={`bg-[#0A070E] ml-auto h-[calc(100vh-48px)] max-sm:w-full max-sm:h-[calc(100vh-88px)] overflow-hidden ${
           isActive ? "w-[calc(100vw-64px)]" : "w-full"
@@ -172,10 +225,10 @@ export default function WorkAreaInfo() {
                             <span
                               className={`${
                                 status === "pending"
-                                  ? "text-green-500"
+                                  ? "text-red-500"
                                   : status === "progressing"
                                   ? "text-yellow-500"
-                                  : "text-sky-500"
+                                  : "text-green-500"
                               }`}
                             >
                               <svg
@@ -190,7 +243,7 @@ export default function WorkAreaInfo() {
                             {status.charAt(0).toUpperCase() + status.slice(1)}
                           </h1>
                         </div>
-                        <div className="flex flex-col gap-2 overflow-y-auto">
+                        <div className="flex flex-col gap-2 overflow-y-auto overflow-x-hidden">
                           {tasks[status as keyof IPromiseHttpResponse].map(
                             (task, index) => (
                               <Draggable
@@ -202,11 +255,11 @@ export default function WorkAreaInfo() {
                                   <div
                                     className={`border-l-8 ${
                                       status === "pending"
-                                        ? "border-green-500"
+                                        ? "border-red-500"
                                         : status === "progressing"
                                         ? "border-yellow-500"
-                                        : "border-sky-500"
-                                    } bg-slate-600 opacity-90 w-[95%] h-[84px] flex items-center justify-center text-xl rounded-lg ml-auto mr-auto`}
+                                        : "border-green-500"
+                                    } bg-slate-600 opacity-90 w-[95%] h-[84px] flex items-center justify-center text-xl rounded-lg ml-auto mr-auto min-h-[80px]`}
                                     ref={provided.innerRef}
                                     {...provided.draggableProps}
                                     {...provided.dragHandleProps}

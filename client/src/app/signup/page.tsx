1a1239b924adb685d0de96ad429d1dbbe065f3a6
Dhomini Pereira <69544085+dhomini-pereira@users.noreply.github.com>
2024-11-03 01:11:50 -0300

    fix: alterando atributo do objeto da resposta da requisição

--- client/src/app/signup/page.tsx
+++ client/src/app/signup/page.tsx
@@ -33,7 +33,7 @@ export default function Page() {
       toast.success("Account created successfully!");
       router.push("/signin");
     } catch (err: any) {
-      toast.error(err.response.data.message);
+      toast.error(err.response.data.error);
       return reset();
     } finally {
       loading.toggle();

cead3c20891faae133f2cf183d3eca093fda4894
dhpe <dhpe@brasilcap.com.br>
2024-11-04 22:46:00 -0300

    feat: implementado reset de senha

--- server/src/controllers/auth/reset_password.py
+++ server/src/controllers/auth/reset_password.py
@@ -1,14 +1,23 @@
 import jwt
 import os
+import hashlib
 from flask import jsonify
+from src.models.user import User
 
 SECRET = os.getenv("SECRET_TOKEN_EMAIL")
 
 def reset_password(data):
     try:
         user_data = jwt.decode(data.get('token'), SECRET, algorithms=['HS256'])
+
+        if not data.get('password'):
+            return jsonify({'error': 'Password is required'}), 400
+
+        user = User.select().where(User.id == user_data.get('data').get('id')).first()
+        user.password = hashlib.sha256(data.get('password').encode('utf-8')).hexdigest()
+        user.save()
         
-        return jsonify(user_data.get('data')), 200
+        return jsonify(), 204
     except jwt.ExpiredSignatureError:
         return jsonify({'error': 'Expired token'}), 401
     except jwt.InvalidTokenError:
